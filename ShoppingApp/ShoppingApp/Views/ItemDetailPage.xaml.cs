0a4e71ec895bf4c7f8812746865ddd9d3a10f1cf
leadrat_ios_dev <sachin.c@gharoffice.com>
2023-02-17 10:33:40 +0530

    first commit

---
+++ ShoppingApp/ShoppingApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ShoppingApp.ViewModels;
+
+namespace ShoppingApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
